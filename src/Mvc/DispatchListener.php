68d328c66c01b986439911e72e7fbd855f39af7c
Chris Kruining <c.kruining@fyn.nl>
2019-03-29 14:18:24 +0100

    minor fixes

--- src/Mvc/DispatchListener.php
+++ src/Mvc/DispatchListener.php
@@ -5,18 +5,18 @@ namespace CPB\Extensions\Zend\Mvc
     use Zend\Router\RouteMatch;
     use Zend\Mvc\Controller\ControllerManager;
     use Zend\Mvc\MvcEvent;
-    
+
     class DispatchListener extends \Zend\Mvc\DispatchListener
     {
         protected $controllerManager;
-        
+
         public function __construct(ControllerManager $controllerManager)
         {
             $this->controllerManager = $controllerManager;
-            
+
             parent::__construct($controllerManager);
         }
-    
+
         public function onDispatch(MvcEvent $e)
         {
             $routeMatch        = $e->getRouteMatch();
@@ -24,14 +24,14 @@ namespace CPB\Extensions\Zend\Mvc
                 ? $routeMatch->getParam('controller', 'not-found')
                 : 'not-found';
             $controllerManager = $this->controllerManager;
-    
+
             if(class_exists($controllerName) && !$controllerManager->has($controllerName))
             {
                 $controllerManager->setAllowOverride(true);
                 $controllerManager->setInvokableClass($controllerName);
                 $controllerManager->setAllowOverride(false);
             }
-            
+
             return parent::onDispatch($e);
         }
     }
--- src/Mvc/Service/ControllerManagerFactory.php
+++ src/Mvc/Service/ControllerManagerFactory.php
@@ -32,9 +32,6 @@ class ControllerManagerFactory implements FactoryInterface
      */
     public function __invoke(ContainerInterface $container, $name, array $options = null)
     {
-        if ($options) {
-            return new ControllerManager($container, $options);
-        }
-        return new ControllerManager($container);
+        return new ControllerManager($container, $options ?? []);
     }
 }
